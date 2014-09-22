283977fb846882087c19d234795f5d66e418cbb9
kiyko <iigorkiyko@gmail.com>
2014-09-22 10:07:30 +0300

    Add unit tests for requests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from unittest import TestCase
+from django.test import Client
+
+
+class RequestTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_data(self):
+        response = self.client.get('/data/users/')
+        #
+        self.assertEqual(response.status_code, 200)
+        #
+        content = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(content['id'], 'users')
+
+    def test_create(self):
+        response = self.client.post('/create/users/',
+                                    {'name': 'UnitTest',
+                                     'paycheck': 10,
+                                     'date_joined': '2014-01-01'})
+        #
+        self.assertEqual(response.status_code, 200)
+        #
+        content = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(content['msg'], 'Model created')
+        self.assertListEqual(content['values'][1:],
+                             ['UnitTest', 10, '2014-01-01'])
+
+    def test_update(self):
+        def _first_row():
+            response = self.client.get('/data/users/')
+            #
+            content = json.loads(response.content.decode("utf-8"))
+            return content['values'][0]
+
+        pk = _first_row()[0]
+        #
+        response = self.client.post('/update/users/{}/'.format(pk),
+                                    {'date_joined': '2014-01-02'})
+        #
+        self.assertEqual(response.status_code, 200)
+        #
+        content = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(content['msg'], 'Model updated')
+        self.assertEqual(content['pk'], pk)
+        #
+        date = _first_row()[3]
+        self.assertEqual(date, '2014-01-02')
